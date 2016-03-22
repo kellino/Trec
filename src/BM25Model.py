783fb97e6f53e2f3b60fb3bc332a0a5bfa40a8a6
David Kelly <thedavidkelly@hotmail.com>
2016-03-22 18:20:26 +0000

    produces the wrong answer

--- src/BM25Model.py
+++ src/BM25Model.py
@@ -1,124 +1,127 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/python2.7
 import math
 import subprocess
 
-DEBUG = False
-
 doc_collection = []
 query_collection = []
+idfs = dict()
+term_frequencies = dict()
 
 
-class Docs():
-    def __init__(self, docID=None, vector=None):
+class Doc():
+    def __init__(self, docID=None, terms=None,
+                 term_frequency=None, doc_length=None):
         self.docID = docID
-        self.vector = vector
+        self.terms = terms
+        self.term_frequency = term_frequency
+        self.doc_length = doc_length
 
 
-class Queries():
-    def __init__(self, queryNo=None, vector=None):
+class Query():
+    def __init__(self, queryNo=None, terms=None,
+                 term_frequency=None):
         self.queryNo = queryNo
-        self.vector = vector
-
-
-def parse_doc_vectors(filename):
-    """ reads the document vectors into a python list """
-    for line in filename:
-        new = Docs()
-        parts = line.split()
-        new.docID = parts[0]
-        vectors = []
-        for part in parts[1:]:
-            if ':' in part:
-                nums = part.split(':')
-                vectors.append((int(nums[0]), int(nums[1])))
-        new.vector = vectors
-        doc_collection.append(new)
-
-
-def parse_query_vectors(filename):
-    for line in filename:
-        new = Queries()
-        parts = line.split()
-        new.queryNo = parts[0]
-        vectors = []
-        for part in parts[1:]:
-            if ':' in part:
-                nums = part.split(':')
-                vectors.append((int(nums[0]), int(nums[1])))
-        new.vector = vectors
-        query_collection.append(new)
-
-
-def get_average_doc_length(N):
-    """ gets the average length of a document where N is the
-        size of the collection """
-    total_words_in_collection = 0
-    for doc in doc_collection:
-        total_words_in_collection += sum([x for (i, x) in doc.vector])
-    return total_words_in_collection / N
-
-
-def calc_nqi(term):
-    """ gets the number of documents containing 'term' """
-    nqi = 0
-    for doc in doc_collection:
-        nqi += sum([x for (i, x) in doc.vector if term == i])
-    return nqi
-
-
-def calc_idf(nqi, N):
-    """ gets the inverse document frequenct of term nqi
-        sets this to 0 if a negative number is returned """
-    div = (N - nqi + 0.5) / nqi + 0.5
-    idf = math.log(div, 2)
-    if idf >= 0:
-        return idf
-    else:
-        return 0
-
-
-def calc_bm25(query, doc, avdl, N):
-    """ calculates the bm25 for a query vector of a document in collection """
-    b = 0.75
-    # k = 1.5
-    K = (1 - b + b * (len(doc.vector) / avdl))
-    idfs = []
-    print K
-    query_terms = [i for (i, q) in query.vector]    # dict, not frequency
-    print "query num = {} query_terms vector = {}".format(
-        query.queryNo, query_terms)
-    doc_vec = [i for (i, x) in doc.vector]
-    for q in query_terms:
-        if q in doc_vec:
-            idf = calc_idf(q, N)
-            idfs.append(idf)
-    print("idfs is/are {}".format(idfs))
+        self.terms = terms
+        self.term_frequency = term_frequency
 
 
 def find_file(search, extension):
-    # not very portable solution to finding the files on the system
-    # but it will do
     filepath = subprocess.check_output(
         'find ~/ -type f -name "{}" | grep -i {}'.format(
             extension, search), shell=True)
     return filepath
 
 
+def parse_file(filename, obj_type, array):
+    for line in filename:
+        parts = line.split()
+        if obj_type == "query":
+            new = Query()
+            new.queryNo = parts[0]
+        else:
+            new = Doc()
+            new.docID = parts[0]
+        terms = []
+        term_frequency = []
+        for part in parts[1:]:
+            if ':' in part:
+                nums = part.split(':')
+                terms.append(int(nums[0]))
+                term_frequency.append(int(nums[1]))
+        new.terms = terms
+        new.term_frequency = term_frequency
+        array.append(new)
+
+
+def get_doc_length():
+    for doc in doc_collection:
+        doc.doc_length = sum(doc.term_frequency)
+
+
+def get_average_doc_length():
+    total = 0
+    for doc in doc_collection:
+        total += doc.doc_length
+    return total / len(doc_collection)
+
+
+def get_term_totals():
+    # very wasteful this...
+    for doc in doc_collection:
+        for i in range(len(doc.terms)):
+            if doc.terms[i] in term_frequencies:
+                term_frequencies[doc.terms[i]] += doc.term_frequency[i]
+            else:
+                term_frequencies[doc.terms[i]] = 0
+
+
+def calculate_idf_of_term(query, N):
+    for q in query.terms:
+        nqi = term_frequencies.get(q)
+        if ((N - nqi + 0.5 / nqi + 0.5)) >= 0:
+            idfs[q] = math.log((N - nqi + 0.5 / nqi + 0.5), 2)
+        else:
+            idfs[q] = 0.0
+
+
+def calculate_bm25(query, avdl):
+    k = 1.5
+    b = 0.75
+    q_vec = [q for q in query.terms]
+    idf_of_q = [idfs.get(q) for q in q_vec]
+    for doc in doc_collection:
+        doc_vec = []        # holds the frequencies of the search terms
+        for q in q_vec:
+            if q in doc.terms:
+                doc_vec.append(doc.term_frequency[doc.terms.index(q)])
+        try:
+            bm25 = sum(map((lambda x, y: x * (y * (k+1) / y + k * (1 - b + b * (doc.doc_length / avdl)))), idf_of_q, doc_vec))
+            print query.queryNo, doc.docID, bm25
+        except:
+            pass
+
+
 if __name__ == '__main__':
     try:
-        query_vectors = open(find_file("query_term_vectors", "*.dat").strip())
-        term_vectors = open(
-            find_file("document_term_vectors", "*.dat").strip())
-        parse_doc_vectors(term_vectors)
-        parse_query_vectors(query_vectors)
-        N = len(doc_collection)
-        avdl = get_average_doc_length(N)
-        for query in query_collection:
-            calc_bm25(query, doc_collection[0], avdl, N)
+        query_file = open(find_file("query_term_vectors", "*.dat").strip())
+        doc_file = open(find_file("document_term_vectors", "*.dat").strip())
+        # read the query file into the query_collection list
+        parse_file(query_file, "query", query_collection)
+        # read the doc file into the doc_collection list
+        parse_file(doc_file, "doc", doc_collection)
     except:
         pass
     finally:
-        if query_vectors is not None:
-            query_vectors.close()
-        if term_vectors is not None:
-            term_vectors.close()
+        # close open files
+        if query_file is not None:
+            query_file.close()
+        if doc_file is not None:
+            doc_file.close()
+    get_doc_length()
+    avdl = get_average_doc_length()
+    get_term_totals()
+    N = len(doc_collection)
+    for query in query_collection:
+        calculate_idf_of_term(query, N)
+    for query in query_collection:
+        calculate_bm25(query, avdl)
