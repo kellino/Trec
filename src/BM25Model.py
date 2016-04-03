a8d665693881d4d32bc70515d551584d5ebfdf07
David Kelly <thedavidkelly@hotmail.com>
2016-04-03 08:40:34 +0100

    completely rewritten

--- src/BM25Model.py
+++ src/BM25Model.py
@@ -1,99 +1,83 @@
-#!/usr/bin/python2.7
-from math import log
-from filehandler import FileHandler
-from sys import exit
+#!/usr/bin/env python2.7
+import numpy as np
 from collections import OrderedDict
-from operator import itemgetter
 
-doc_collection = dict()
-query_collection = OrderedDict()
-idfs = dict()
-term_totals = dict()
+query_nums = []
+docIDs = []
+bm25 = []
 
+with open(r'/home/david/Documents/data_retrieval'
+          r'/coursework/BM25b0.75_0.res') as ifile:
+    for line in ifile:
+        tokens = line.strip().split()
+        query_nums.append(int(tokens[0]))
+        docIDs.append(tokens[2])
+        bm25.append(float(tokens[4]))
 
-class BM25():
+query_nums = np.array(query_nums)
+docIDs = np.array(docIDs)
+bm25 = np.array(bm25)
 
-    def get_doc_lengths(self):
-        for k, doc in doc_collection.iteritems():
-            doc.doc_length = sum(doc.term_frequencies)
-
-    def get_average_doc_length(self):
-        # sum the doc lengths of everything in the dictionary, and divide
-        # by length of dictionary
-        return (sum([v.doc_length for k, v in doc_collection.iteritems()]) /
-                len(doc_collection))
-
-    def get_term_totals(self):
-        # gets the total number of occurrences of each term in the collection
-        # slightly wasteful of memory, as it is really only necessary to
-        # get the terms totals of query items
-        for k, doc in doc_collection.iteritems():
-            for i in range(len(doc.terms)):
-                # if a term appears in the document, increment the count
-                if doc.terms[i] in term_totals:
-                    term_totals[doc.terms[i]] += 1
-                # if term not in dictionary, initialize with the term frequency
+total_words = 0
+docs = dict()
+term_appearances = dict()
+with open(r'/home/david/Documents/data_retrieval'
+          r'/coursework/document_term_vectors.dat') as ifile:
+    for line in ifile:
+        terms = []
+        term_frequencies = []
+        tokens = line.strip().split()
+        for token in tokens[1:]:
+            if ':' in token:
+                nums = token.strip().split(':')
+                terms.append(int(nums[0]))
+                term_frequencies.append(int(nums[1]))
+                if int(nums[0]) in term_appearances:
+                    term_appearances[int(nums[0])] += 1
                 else:
-                    term_totals[doc.terms[i]] = 1
+                    term_appearances[int(nums[0])] = 1
+        # add the documents vectors to the docs dictionary
+        docs[tokens[0]] = np.array([terms, term_frequencies])
+        total_words += np.sum(term_frequencies)
 
-    def calculate_idf_of_terms(self, N):
-        # calculate the inverse document frequency of each term in the query
-        # IDFt = log10(N/nt)
-        for k, query in query_collection.iteritems():
-            for q in query.terms:
-                nt = term_totals.get(q)
-                # the log base is irrelevant, but base 2 seems to run slightly
-                # faster
-                idfs[q] = log((N / nt), 2)
-
-    def calculate_bm25(self, query, avdl, k, b):
-        order = []
-        for i, doc in doc_collection.iteritems():
-            # f(qi, D)
-            t_freq = []
-            for q in query.terms:
-                if q in doc.terms:
-                    t_freq.append(doc.term_frequencies[doc.terms.index(q)])
-                else:
-                    t_freq.append(0)
-            # IDF(qi)
-            idf_list = []
-            for j in range(len(query.terms)):
-                idf_list.append(idfs.get(query.terms[j]))
-            # this version of bm25 was taken from Introduction to Information
-            # Retrieval, C. Manning et al
-            s = sum(map((lambda idf, tf: (k + 1) * tf / k * (
-                (1 - b) + b * (doc.doc_length / avdl)) + tf),
-                idf_list, t_freq))
-            order.append([query.ID, doc.ID, s])
-        order.sort(key=itemgetter(2), reverse=True)
-        for o in order:
-            print o[0], o[1], o[2]
+query_vecs = OrderedDict()
+with open(r'/home/david/Documents/data_retrieval'
+          r'/coursework/query_term_vectors.dat') as ifile:
+    for line in ifile:
+        terms = []
+        term_frequencies = []
+        tokens = line.strip().split()
+        for token in tokens[1:]:
+            if ':' in token:
+                nums = token.strip().split(':')
+                terms.append(int(nums[0]))
+                term_frequencies.append(int(nums[1]))
+        query_vecs[int(tokens[0])] = np.array([terms, term_frequencies])
 
 
-if __name__ == '__main__':
-    try:
-        f = FileHandler()
-        b = BM25()
-        query_file = open(f.find_file("query_term_vectors", "*.dat"))
-        doc_file = open(f.find_file("document_term_vectors", "*.dat"))
-        f.fill_dictionary(doc_file, doc_collection)
-        f.fill_dictionary(query_file, query_collection)
-    except:
-        print("unexpected error reading from files")
-        exit()
-    finally:
-        # close open files
-        f.close_file(query_file)
-        f.close_file(doc_file)
-    b.get_doc_lengths()
-    avdl = b.get_average_doc_length()
-    b.get_term_totals()
-    N = len(doc_collection)
-    b.calculate_idf_of_terms(N)
-    for k, v in query_collection.iteritems():
-        b.calculate_bm25(v, avdl, 1.5, 0.75)
+def calc_bm25(query_vec, doc_vec, b, k):
+    L = np.sum(doc_vec[1]) / avdl
+    K = k * ((1 - b) + (b * L))
+    vec_intersection = np.in1d(doc_vec[0], query_vec[0])
+    if vec_intersection.any():
+        term_frequencies = [term_appearances.get(y)
+                            for y in doc_vec[0][vec_intersection]]
+        bm25 = np.sum(map((lambda x, y: (x / x + K) *
+                           (np.log2((len(docs) - y + 0.5) / (y + 0.5)))),
+                          doc_vec[1][vec_intersection], term_frequencies))
+    else:
+        bm25 = 0.0
+        term_frequencies = []
+    return L, K, term_frequencies, bm25
 
-    cache = open('./idfs_cached', 'a')
-    cache.write(str(idfs))
-    cache.close
+
+avdl = total_words / float(len(docs))
+for query, query_vec in query_vecs.iteritems():
+    results = []
+    for doc, doc_vec in docs.iteritems():
+        L, K, term_frequencies, bm25 = calc_bm25(query_vec, doc_vec, 0.75, 1.5)
+        results.append((doc, bm25))
+    rs = sorted(results, key=lambda bm25: bm25[1])
+    rs.reverse()
+    for i in range(100):
+        print query, "Q0", rs[i][0], i+1, rs[i][1], "BM25b0.75"
