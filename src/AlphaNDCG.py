f70f910a2b2f1d6dd886ea0206a981323c2a86a4
David Kelly <thedavidkelly@hotmail.com>
2016-04-05 19:38:54 +0100

    nothing yet

---
+++ src/AlphaNDCG.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2.7
+# import numpy as np
+# import pandas as pd
+
+
+with open(r'/home/david/Documents/data_retrieval'
+          r'/coursework/qrels.ndeval.txt', 'r') as f:
+    pass
