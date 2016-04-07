447d0823d35caf962eecb918958a27d859e88981
David Kelly <thedavidkelly@hotmail.com>
2016-04-07 13:29:38 +0100

    piccoli passi

--- src/AlphaNDCG.py
+++ src/AlphaNDCG.py
@@ -1,43 +1,58 @@
 #!/usr/bin/env python2.7
-
 import numpy as np
-from itertools import islice
+# from itertools import islice
 
 
 class AlphaNDCG():
     def __init__(self):
-        self.queries = []
-        self.sub_topics = []
+        # parts belonging to the bm25 results
         self.docIDs = []
-        self.judgments = []
+        self.query_no = []
+        # parts belonging to the ndeval judgements
+        self.ndeval_query = []
+        self.ndeval_docID = []
+        self.subtopic = []
+        self.judgement = []
 
-    def get_ndevals(self):
+    def get_bm25_scores(self):
+        with open('./BM25b0.75.res') as ifile:
+            for line in ifile:
+                tokens = line.strip().split()
+                self.docIDs.append(tokens[2])
+                self.query_no.append(int(tokens[0]))
+        self.docIDs = np.array(self.docIDs)
+        self.query_no = np.array(self.query_no)
+
+    def get_qrels(self):
         with open(r'/home/david/Documents/data_retrieval'
                   r'/coursework/qrels.ndeval.txt') as ifile:
-            while True:
-                next_n_lines = list(islice(ifile, 5000))
-                if not next_n_lines:
-                    break
-                else:
-                    for line in next_n_lines:
-                        tokens = line.strip().split()
-                        self.queries.append(int(tokens[0]))
-                        self.sub_topics.append(int(tokens[1]))
-                        self.docIDs.append(tokens[2])
-                        self.judgments.append(int(tokens[3]))
-            self.queries = np.array(self.queries)
-            self.sub_topics = np.array(self.sub_topics)
-            self.docIDs = np.array(self.docIDs)
-            self.judgments = np.array(self.judgments)
+            for line in ifile:
+                tokens = line.strip().split()
+                # query number
+                self.ndeval_query.append(int(tokens[0]))
+                # doc ID
+                self.ndeval_docID.append(tokens[2])
+                # subtopic
+                self.subtopic.append(int(tokens[1]))
+                # judgement
+                self.judgement.append(int(tokens[3]))
+        self.ndeval_query = np.array(self.ndeval_query)
+        self.ndeval_docID = np.array(self.ndeval_docID)
+        self.subtopic = np.array(self.subtopic)
+        self.judgement = np.array(self.judgement)
 
+    def run(self, query):
+        # isolate the part of ndeval which belongs to the query number
+        belongs_to_query = (self.ndeval_query == query)
+        collection = self.ndeval_docID[belongs_to_query]
+        for c in collection:
+            is_c = (collection == c)
+            judgements = self.judgement[is_c]
+            print c, judgements
 
 if __name__ == '__main__':
-    A = AlphaNDCG()
-    A.get_ndevals()
-    for query in range(201, 251):
-        is_query = (A.queries == query)
-        docs = A.docIDs[is_query]
-        sub_topics = A.sub_topics[is_query]
-        for i in range(np.min(sub_topics), np.max(sub_topics)+1):
-            relevant = (sub_topics == i)
-            print "query {} subtopic {} docs {}".format(query, i, docs[relevant])
+    a = AlphaNDCG()
+    a.get_bm25_scores()
+    a.get_qrels()
+    for i in range(201, 202):
+        a.run(i)
