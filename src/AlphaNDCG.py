e1d26937c48e889d22fa7f7eb345a0fa866ec22b
David Kelly <thedavidkelly@hotmail.com>
2016-04-08 16:00:58 +0100

    finished

--- src/AlphaNDCG.py
+++ src/AlphaNDCG.py
@@ -52,7 +52,7 @@ class AlphaNDCG():
         for i in range(len(js)):
             # special case for first element
             if i == 0:
-                gain.append(np.sum(js[i][2]))  # * (1 - alpha)**0)
+                gain.append(np.sum(js[i][2]) * (1 - alpha)**0 / np.log2(2))
                 for s, x in enumerate(js[i][2]):
                     seen[s] = x
             else:
@@ -62,13 +62,17 @@ class AlphaNDCG():
                     e = 0
                     for s, x in enumerate(js[i][2]):
                         if x == 1:
-                            e += seen.get(s)
-                            seen[s] += 1
+                            if seen.get(s) is not None:
+                                e += seen.get(s)
+                                seen[s] += 1
                     new_gain = gain[-1] + ((np.sum(js[i][2]) * (1 - alpha)**e)) / np.log2(2 + i)
                     gain.append(new_gain)
         return np.sum(gain[:k])
 
     def run(self, query, alpha, ks):
+        # isolate the top 100 docs for query
+        top_100 = (self.query_no == query)
+        docs = self.docIDs[top_100]
         # isolate the part of ndeval which belongs to the query number
         belongs_to_query = (self.ndeval_query == query)
         collection = self.ndeval_docID[belongs_to_query]
@@ -77,43 +81,42 @@ class AlphaNDCG():
         # the following form docID (as ranked by bm25) [1 1 1 1 1 0]
         # = judgements for each subtopic, in order
         js = []
-        for c in collection:
-            is_c = (collection == c)
-            judgements = judgement_list[is_c]
-            judgements[judgements > 1] = 1
-            # tuple of query num, doc id and list of binary judgements where
-            # the index represents the subtopic
-            js.append((query, c, judgements))
-        # # main loop
+        for d in docs:
+            ind = np.where(collection == d)
+            for i in ind:
+                juds = judgement_list[i]
+                juds = np.array(juds)
+                juds[juds > 1] = 1
+                nans = np.isnan(juds)
+                juds[nans] = 0
+            js.append((query, d, juds))
         at_k = []
         for k in ks:
             dcg = self.calc_alpha_dcg(js, alpha, k)
             ideal = sorted(js, key=lambda (a, b, c): sum(c))
             ideal.reverse()
             idcg = self.calc_alpha_dcg(ideal, alpha, k)
-            at_k.append(dcg / idcg)
+            if idcg > 0:
+                at_k.append(dcg / idcg)
+            else:
+                at_k.append(0.0)
         return at_k
 
 
 if __name__ == '__main__':
     a = AlphaNDCG()
-    a.get_scores('./BM25b0.75.res')
+    a.get_scores('./PortfolioScoringBP4.res')
     a.get_qrels()
     ks = [1, 5, 10, 20, 30, 40, 50]
     totals = []
-    for i in range(202, 203):
-        at_k = a.run(i, 0.1, ks)
-        totals.append(at_k)
-    totals = np.array(totals)
-    means = np.mean(totals, axis=0)
-
-    # totals = []
-    # for i in range(len(means)):
-        # print "k {} mean {}".format(ks[i], means[i])
-    # for i in range(201, 251):
-        # at_k = a.run(i, 0.5, ks)
-        # totals.append(at_k)
-    # totals = np.array(totals)
-    # means = np.mean(totals, axis=0)
-    # for i in range(len(means)):
-        # print "k {} mean {}".format(ks[i], means[i])
+    for i in range(201, 251):
+        if i in a.ndeval_query:
+            at_k = a.run(i, 0.1, ks)
+            totals.append(at_k)
+        means = np.mean(totals, axis=0)
+    with open('mmr_ndcg.txt', 'a') as f:
+        f.write('MMR\n')
+        f.write('lambda = 0.25\n')
+        f.write('alpha | K |  alpha-NDCG@K\n')
+        for j in range(len(ks)):
+            f.write('0.1\t{}\t{:.3f}\n'.format(ks[j], means[j]))
